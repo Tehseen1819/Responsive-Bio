4964e5c52517f46af34f90ef83557e1a2cc90bf2
tehseen1819 <tehseenahmad005@gmail.com>
2024-10-06 11:28:40 +0530

    change light-dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'light-body': '#F9F7F7',
+        'light-btn' : '#112D4E',
+        'light-bg-btn': '#DBE2EF',
+        'dark-body': '#181C14',
+        'dark-text': '#ECDFCC',
+        'dark-btn': '#3C3D37'
+
+        
+      }
+    },
   },
   plugins: [],
   darkMode: 'selector',
