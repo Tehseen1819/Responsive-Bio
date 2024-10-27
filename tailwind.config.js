c2adbcdea3b3d3c1d4396ff958f3155c1bf1bf96
tehseen1819 <tehseenahmad005@gmail.com>
2024-10-27 22:45:33 +0530

    fix buttons

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,15 +3,16 @@ module.exports = {
   content: ["./*.{html,js}"],
   theme: {
     extend: {
+      boxShadow:{
+        'custom': 'inset 0px 5px 15px rgba(0, 0, 0, 0.4), inset 0px -5px 15px rgba(255, 255, 255, 0.4)',
+      },
       colors: {
         'light-body': '#F9F7F7',
         'light-text' : '#112D4E',
         'light-btn': '#DBE2EF',
         'dark-body': '#181C14',
         'dark-text': '#ECDFCC',
-        'dark-btn': '#3C3D37',
-
-        
+        'dark-btn': '#3C3D37',        
       }
     },
   },
